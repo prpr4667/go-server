ef17cb213100e585442e57b0f88387da089c3b19
pratikpr <89504885+prpr4667@users.noreply.github.com>
2023-04-26 20:15:07 -0600

    basic form response

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello")
+}
+
+func fomrHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error in parsing form: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully submitted!")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Hi %v from %v", name, address)
+}
 
 func main() {
-	fmt.Println("Hello")
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", fomrHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server started at port 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
